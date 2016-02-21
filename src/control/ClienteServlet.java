3083d7cc5806e66147371b7ccf70d4f82688593a
Brunapf <brunapflor@gmail.com>
2016-02-21 15:05:18 -0300

    Correção

--- src/control/ClienteServlet.java
+++ src/control/ClienteServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class AtendimentoServlet
  */
 @WebServlet("/AtendimentoServlet")
-public class AtendimentoServlet extends HttpServlet {
+public class ClienteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AtendimentoServlet() {
+    public ClienteServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
