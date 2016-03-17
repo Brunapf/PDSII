ccdf47c5505a5e64b7337dc7bda25dda711ffce3
Brunapf <brunapflor@gmail.com>
2016-03-17 03:45:28 -0300

    adicionando formulario simples

--- src/control/ClienteServlet.java
+++ src/control/ClienteServlet.java
@@ -1,8 +1,8 @@
 package control;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AtendimentoServlet
  */
-@WebServlet("/AtendimentoServlet")
+
 public class ClienteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
